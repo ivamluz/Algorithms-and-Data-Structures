e65c8ceda4160076786daf0e7966f58fb90cbb45
Ivam Luz <iluz@ciandt.com>
2018-11-04 22:58:58 -0200

    Implement solution to Reverse Strings problem

---
+++ algorithms/0003-ReverseStrings/README.md
@@ -0,0 +1,13 @@
+# Reverse Strings
+
+Write a function that takes a string as input and returns the string reversed. ([Source](https://leetcode.com/problems/reverse-string/))
+
+## Example 1:
+**Input:** "hello"
+
+**Output:** "olleh"
+
+## Example 2:
+**Input:** "A man, a plan, a canal: Panama"
+
+**Output:** "amanaP :lanac a ,nalp a ,nam A"
---
+++ algorithms/0003-ReverseStrings/test_reverse_string.py
@@ -0,0 +1,22 @@
+import unittest
+from reverse_string import reverse
+
+class TestReverseString(unittest.TestCase):
+
+    def test_empty_string(self):
+        self.assertEqual(reverse(''), '')
+
+    def test_none(self):
+        self.assertEqual(reverse(None), None)
+
+    def test_single_char(self):
+        self.assertEqual(reverse('a'), 'a')
+
+    def test_small_string(self):
+        self.assertEqual(reverse('abc'), 'cba')
+
+    def test_larger_string(self):
+        self.assertEqual(reverse('A man, a plan, a canal: Panama'), 'amanaP :lanac a ,nalp a ,nam A')
+
+if __name__ == '__main__':
+    unittest.main()

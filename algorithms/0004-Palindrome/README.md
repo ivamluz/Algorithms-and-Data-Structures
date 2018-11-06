02dff947f62d280d1c029295aa5d64f5470eaf5a
Ivam Luz <iluz@ciandt.com>
2018-11-05 23:04:23 -0200

    Create skeleton for Palindrome problem solution

---
+++ algorithms/0004-Palindrome/README.md
@@ -0,0 +1,28 @@
+# Is Palindrome
+
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+## Input Format
+
+A single line which contains the input string.
+
+## Output Format
+
+A single line which contains YES or NO in uppercase.
+
+## Sample Input
+
+A man, a plan, a canal: Panama
+
+## Sample Output
+
+True
+
+## Explanation
+
+Have you consider that the string might be empty? This is a good question to ask during an interview.
+
+For the purpose of this problem, we define empty string as valid palindrome.
+
+
+**Source:** [HackerRank](https://www.hackerrank.com/contests/doyoulikeit/challenges/doyoulikeit)
---
+++ algorithms/0004-Palindrome/test_palindrome.py
@@ -0,0 +1,28 @@
+import unittest
+from palindrome import is_palindrome
+
+class TestIsPalindrome(unittest.TestCase):
+
+    def test_phrase_with_spaces(self):
+        self.assertTrue(is_palindrome('A man, a plan, a canal: Panama'))
+
+    def test_none(self):
+        self.assertFalse(is_palindrome(None))
+
+    def test_empty_string(self):
+        self.assertTrue(is_palindrome(''))
+
+    def test_palindrome_with_even_length(self):
+        self.assertTrue(is_palindrome('abba'))
+        
+    def test_non_palindrome_with_even_length(self):
+        self.assertFalse(is_palindrome('abbc'))
+
+    def test_palindrome_with_odd_length(self):
+        self.assertTrue(is_palindrome('abcba'))
+        
+    def test_non_palindrome_with_odd_length(self):
+        self.assertFalse(is_palindrome('abcbc'))
+
+if __name__ == '__main__':
+    unittest.main()

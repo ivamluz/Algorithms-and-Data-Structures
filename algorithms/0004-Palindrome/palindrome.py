0d5e2ed20cd5a4b027e1961ac6c9c453ed798cb8
Ivam Luz <iluz@ciandt.com>
2018-11-06 21:54:33 -0200

    First implementation of Palindrome (still need to analize complexy and refactor if necessary)

--- algorithms/0004-Palindrome/palindrome.py
+++ algorithms/0004-Palindrome/palindrome.py
@@ -1,2 +1,13 @@
+import re
+
 def is_palindrome(str):
-    return False
+    if str is None:
+        return False
+
+    cleanedStr = re.sub(
+        r"[^a-z0-9]", 
+        '', 
+        str.lower()
+    )
+
+    return cleanedStr == cleanedStr[::-1]

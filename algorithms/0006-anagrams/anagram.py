7575dab4dbd29f6e00fa179f558c1f47a8504378
Ivam Luz <iluz@ciandt.com>
2018-11-08 20:28:02 -0200

    Implement anagram checking for empty and None strings

--- algorithms/0006-anagrams/anagram.py
+++ algorithms/0006-anagrams/anagram.py
@@ -1,2 +1,6 @@
 def is_anagram(str1, str2):
-  return False
+  if not str1:
+    return False
+
+  if not str2:
+    return False
--- algorithms/0006-anagrams/test_anagram.py
+++ algorithms/0006-anagrams/test_anagram.py
@@ -5,5 +5,20 @@ class TestIsAnagram(unittest.TestCase):
   def test_both_strings_empty(self):
     self.assertFalse(is_anagram('', ''))
 
+  def test_first_string_empty(self):
+    self.assertFalse(is_anagram('', 'earth'))
+
+  def test_second_string_empty(self):
+    self.assertFalse(is_anagram('earth', ''))
+
+  def test_both_strings_none(self):
+    self.assertFalse(is_anagram(None, None))
+
+  def test_first_string_none(self):
+    self.assertFalse(is_anagram(None, 'earth'))
+
+  def test_second_string_none(self):
+    self.assertFalse(is_anagram('earth', None))
+
 if __name__ == '__main__':
   unittest.main()

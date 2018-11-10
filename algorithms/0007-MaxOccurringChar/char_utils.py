e2cc49596b0c4014b275f70fabe75cb47810ce9b
Ivam Luz <iluz@ciandt.com>
2018-11-10 12:44:21 -0200

    Finished implementation for 0007-MaxOccurringChar challenge solution

--- algorithms/0007-MaxOccurringChar/char_utils.py
+++ algorithms/0007-MaxOccurringChar/char_utils.py
@@ -1,3 +1,23 @@
-def max_occurring_chars(str):
+def most_occurring_chars(str):
   if not str:
     return None
+
+  counts = {}
+  for char in str:
+    if char in counts:
+      counts[char] += 1
+    else:
+      counts[char] = 1
+
+  max = 0
+  most_occurring_chars_map = {}
+  for char, count in sorted(counts.iteritems(), key=lambda (k,v): (v,k), reverse=True):
+    if count > max:
+      max = count
+
+    if (count == max):
+      most_occurring_chars_map[char] = count
+    else:
+      break
+
+  return most_occurring_chars_map
--- algorithms/0007-MaxOccurringChar/test_max_occurring_chars.py
+++ algorithms/0007-MaxOccurringChar/test_max_occurring_chars.py
@@ -1,12 +1,21 @@
 import unittest
-from char_utils import max_occurring_chars
+from char_utils import most_occurring_chars
 
-class TestMaxOccurringChars(unittest.TestCase):
+class TestMostOccurringChars(unittest.TestCase):
   def test_none_string(self):
-    self.assertIsNone(max_occurring_chars(None))
+    self.assertIsNone(most_occurring_chars(None))
 
   def test_empty_string(self):
-    self.assertIsNone(max_occurring_chars(''))
+    self.assertIsNone(most_occurring_chars(''))
+
+  def test_string_with_spaces_only(self):
+    self.assertEquals(most_occurring_chars('   '), {' ': 3})
+
+  def test_single_most_occurring_char(self):
+    self.assertEquals(most_occurring_chars('tutorial horizon'), {'o': 3})
+
+  def test_multiple_most_occurring_chars(self):
+    self.assertEquals(most_occurring_chars('abcabcdefabcab'), {'a': 4, 'b': 4})
 
 if __name__ == '__main__':
   unittest.main()

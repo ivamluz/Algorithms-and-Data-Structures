ef0da1b2c1d27ed4cf4ba54387b553ad0aba6656
Ivam Luz <iluz@ciandt.com>
2018-11-11 10:54:29 -0200

    Create skeleton for 0008-ArrayChunk solution

---
+++ algorithms/0008-ArrayChunk/array_utils.py
@@ -0,0 +1,2 @@
+def chunk(arr, size):
+  pass
---
+++ algorithms/0008-ArrayChunk/test_array_chunk.py
@@ -0,0 +1,9 @@
+import unittest
+from array_utils import chunk
+
+class TestArrayChunk(unittest.TestCase):
+  def test_none_array(self):
+    pass
+
+if __name__ == '__main__':
+  unittest.main()

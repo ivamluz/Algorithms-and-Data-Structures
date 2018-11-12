9f363af3f6b6cb9fb3336e9bc8e2dec4536f6d75
Ivam Luz <iluz@ciandt.com>
2018-11-12 21:11:16 -0200

    Implement solution for 0009-Capitalize challenge

--- algorithms/0009-Capitalize/string_utils.py
+++ algorithms/0009-Capitalize/string_utils.py
@@ -1,2 +1,27 @@
+import re
+
 def capitalize(str):
-  pass
+  if not str:
+    return str
+
+  alphanum_regex = re.compile("\w")
+
+  capitalized_chars_list = []
+  idx = 0
+
+  str_chars_list = list(str)
+  while idx < len(str_chars_list):
+    char = str_chars_list[idx]
+
+    if idx == 0:
+      capitalized_chars_list.append(char.upper())
+    else:
+      previous_char = str_chars_list[idx - 1]
+      if re.match(alphanum_regex, previous_char):
+        capitalized_chars_list.append(char.lower())
+      else:
+        capitalized_chars_list.append(char.upper())
+
+    idx += 1
+
+  return "".join(capitalized_chars_list)
--- algorithms/0009-Capitalize/test_capitalize.py
+++ algorithms/0009-Capitalize/test_capitalize.py
@@ -2,8 +2,47 @@ import unittest
 from string_utils import capitalize
 
 class TestCapitalize(unittest.TestCase):
-  def test_none_array(self):
+  def test_none_string(self):
     self.assertEquals(capitalize(None), None)
+
+  def test_empty_string(self):
+    self.assertEquals(capitalize(''), '')
+
+  def test_only_spaces(self):
+    self.assertEquals(capitalize('   '), '   ')
+
+  def test_only_numbers(self):
+    self.assertEquals(capitalize('123'), '123')
+
+  def test_single_string_uppercased(self):
+    self.assertEquals(capitalize('ABC'), 'Abc')
+
+  def test_multiple_strings_uppercased(self):
+    self.assertEquals(capitalize('ABC DEF'), 'Abc Def')
+
+  def test_single_string_lowercased(self):
+    self.assertEquals(capitalize('abc'), 'Abc')
+
+  def test_single_string_starting_with_numbers(self):
+    self.assertEquals(capitalize('123abc'), '123abc')
+
+  def test_single_string_starting_with_letters_containing_numbers(self):
+    self.assertEquals(capitalize('abc123'), 'Abc123')
+
+  def test_multiple_strings_starting_with_letters_containing_numbers(self):
+    self.assertEquals(capitalize('abc123 def456'), 'Abc123 Def456')
+
+  def test_multiple_strings_starting_with_letters_containing_numbers_and_vice_versa(self):
+    self.assertEquals(capitalize('abc123 456def'), 'Abc123 456def')
+
+  def test_multiple_spaces(self):
+    self.assertEquals(capitalize('abc   def'), 'Abc   Def')
+
+  def test_with_separators_other_than_spaces(self):
+    self.assertEquals(capitalize(u'hello, how are you? i am fine!'), u'Hello, How Are You? I Am Fine!')
+
+  def test_should_lower_case_non_first_chars(self):
+    self.assertEquals(capitalize(u'hEllo, hOw aRe you? i am fine!'), u'Hello, How Are You? I Am Fine!')
 
 if __name__ == '__main__':
   unittest.main()

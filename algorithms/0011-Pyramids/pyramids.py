28d6d8e2d70e918c97c561266e622ee1b9061131
Ivam Luz <iluz@ciandt.com>
2018-11-14 21:47:38 -0200

    Implement solution for 0011-Pyramids

--- algorithms/0011-Pyramids/pyramids.py
+++ algorithms/0011-Pyramids/pyramids.py
@@ -1,2 +1,23 @@
-def pyramids(size):
-  pass
+# lvl |  chars
+# 1   | '   #   '
+# 2   | '  ###  '
+# 3   | ' ##### '
+# 4   | '#######'
+# spaces_before_and_after = levels - lvl
+# chars = (lvl * 2) -1
+
+def pyramids(levels):
+  if levels <= 0:
+    raise ValueError('Levels should be greather than 0.')
+
+  pyramid = ''
+  for level in range(1, levels + 1):
+    spaces = levels - level
+    blocks = (level * 2) - 1
+
+    pyramid += (' ' * spaces) + ('#' * blocks) + (' ' * spaces)
+
+    if (level < levels):
+      pyramid += '\n'
+
+  return pyramid
--- algorithms/0011-Pyramids/test_pyramids.py
+++ algorithms/0011-Pyramids/test_pyramids.py
@@ -1,7 +1,32 @@
 import unittest
+from pyramids import pyramids
 
 class TestPyramids(unittest.TestCase):
-  pass
+  def test_negative_levels(self):
+    with self.assertRaises(ValueError):
+      self.assertRaises(pyramids(-1))
+
+  def test_zero_levels(self):
+    with self.assertRaises(ValueError):
+      self.assertRaises(pyramids(0))
+
+  def test_single_level(self):
+    self.assertEquals(pyramids(1), '#')
+
+  def test_double_level(self):
+    expected_output  = ' # \n'
+    expected_output += '###'
+
+    self.assertEquals(pyramids(2), expected_output)
+
+  def test_multiple_levels(self):
+    expected_output  = '    #    \n'
+    expected_output += '   ###   \n'
+    expected_output += '  #####  \n'
+    expected_output += ' ####### \n'
+    expected_output += '#########'
+
+    self.assertEquals(pyramids(5), expected_output)
 
 if __name__ == '__main__':
   unittest.main()

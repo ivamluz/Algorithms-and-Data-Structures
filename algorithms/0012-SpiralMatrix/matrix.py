d56d6cf859b267ebcf5d7a61d79e90b2fdf55ed6
Ivam Luz <iluz@ciandt.com>
2018-11-15 22:27:16 -0200

    Implement solution for 0012-SpiralMatrix challenge

--- algorithms/0012-SpiralMatrix/matrix.py
+++ algorithms/0012-SpiralMatrix/matrix.py
@@ -1,2 +1,78 @@
 def spiral(size):
-  pass
+
+  # Update this variable to toggle debugging
+  enable_debugging = False
+  def debug_values(action):
+    if not enable_debugging:
+      return
+
+    print "===== %s" % action
+    print "start_column: %s" % start_column
+    print "end_column: %s" % end_column
+    print "current_column: %s" % current_column
+    print "start_row: %s" % start_row
+    print "end_row: %s" % end_row
+    print "current_row: %s" % current_row
+    print "counter: %s" % counter
+    print "max_items: %s" % max_items
+
+
+  if size <= 0:
+    raise ValueError('Size should be greather than 0.')
+
+  start_row = 0
+  start_column = 0
+  end_row = size -1
+  end_column = size - 1
+  current_row = 0
+  current_column = 0
+
+  counter = 0
+  max_items = size * size
+
+  spiral = [[None for column in range(size)] for row in range(size)]
+  while counter < max_items:
+    debug_values("starting while block")
+
+    for current_column in range(start_column, end_column + 1):
+      debug_values("increasing column")
+
+      counter += 1
+      spiral[current_row][current_column] = counter
+
+    start_row += 1
+
+    for current_row in range(start_row, end_row + 1):
+      debug_values("increasing row")
+
+      counter += 1
+      spiral[current_row][current_column] = counter
+
+    end_column -= 1
+
+    for current_column in range(end_column, start_column - 1, -1):
+      debug_values("decreasing column")
+
+      counter += 1
+      spiral[current_row][current_column] = counter
+
+    end_row -= 1
+
+    for current_row in range(end_row, start_row - 1, -1):
+      debug_values("decreasing row")
+
+      counter += 1
+      spiral[current_row][current_column] = counter
+
+    start_column += 1
+
+    debug_values("finishing while block")
+
+  if enable_debugging:
+    print "==== Final status"
+    import pprint
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(spiral)
+    print "counter: %s" % counter
+
+  return spiral
--- algorithms/0012-SpiralMatrix/test_spiral.py
+++ algorithms/0012-SpiralMatrix/test_spiral.py
@@ -3,10 +3,41 @@ from matrix import spiral
 
 class TestSpiral(unittest.TestCase):
   def test_negative_size(self):
-    spiral(-1)
+    with self.assertRaises(ValueError):
+      spiral(-1)
 
   def test_zeroed_size(self):
-    spiral(0)
+    with self.assertRaises(ValueError):
+      spiral(0)
+
+  def test_single_sized_matrix(self):
+    self.assertEquals(spiral(1), [[1]])
+
+  def test_size_two_matrix(self):
+    expected_output = [
+      [1, 2],
+      [4, 3]
+    ]
+    self.assertEquals(spiral(2), expected_output)
+
+  def test_larger_matrix_even_size(self):
+    expected_output = [
+      [1,   2,  3, 4],
+      [12, 13, 14, 5],
+      [11, 16, 15, 6],
+      [10,  9,  8, 7]
+    ]
+    self.assertEquals(spiral(4), expected_output)
+
+  def test_larger_matrix_odd_size(self):
+    expected_output = [
+      [1,   2,  3, 4,  5],
+      [16, 17, 18, 19, 6],
+      [15, 24, 25, 20, 7],
+      [14, 23, 22, 21, 8],
+      [13, 12, 11, 10, 9]
+    ]
+    self.assertEquals(spiral(5), expected_output)
 
 if __name__ == '__main__':
   unittest.main()

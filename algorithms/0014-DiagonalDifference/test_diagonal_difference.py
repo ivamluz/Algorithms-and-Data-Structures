6d2c403f1117d47a12935fe9d0185b5e025f6123
Ivam Luz <iluz@ciandt.com>
2018-11-18 22:26:28 -0200

    Implement solution for 0014-DiagonalDifference

--- algorithms/0014-DiagonalDifference/test_diagonal_difference.py
+++ algorithms/0014-DiagonalDifference/test_diagonal_difference.py
@@ -8,5 +8,22 @@ class TestDiagonalDifference(unittest.TestCase):
   def test_empty_matrix(self):
     self.assertEquals(diagonal_difference([]), 0)
 
+  def test_odd_sized_matrix(self):
+    input = [
+      [11, 2,   4],
+      [4,  5,   6],
+      [10, 8, -12]
+    ]
+    self.assertEquals(diagonal_difference(input), 15)
+
+  def test_even_sized_matrix(self):
+    input = [
+      [11,  2,   4, -5],
+      [4,   5,   6,  0],
+      [10,  8, -12,  7],
+      [7,  13,  -8,  9]
+    ]
+    self.assertEquals(diagonal_difference(input), 3)
+
 if __name__ == '__main__':
   unittest.main()

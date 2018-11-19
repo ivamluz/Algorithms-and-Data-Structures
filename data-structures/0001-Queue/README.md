2bd8dcbef08127a137c2e6253158d37b43c716fe
Ivam Luz <iluz@ciandt.com>
2018-11-19 15:55:23 -0200

    Implement solution for data-structures/0001-Queue

--- data-structures/0001-Queue/README.md
+++ data-structures/0001-Queue/README.md
@@ -7,10 +7,14 @@ Enqueueing ane element should store an element until it is dequeued.
 ## Examples
 ```python
 q = Queue()
+
 q.enqueue(1)
 q.enqueue(3)
 q.enqueue(5)
+
 q.size() # returns 3
+
 q.peek() # returns 5
+
 q.dequeue(); # returns 5 and remove it out of the queue;
 ```
--- data-structures/0001-Queue/queue.py
+++ data-structures/0001-Queue/queue.py
@@ -1,2 +1,47 @@
 class Queue:
-  pass
+  def __init__(self):
+    self._size = 0
+    self._tail = None
+    self._head = None
+    self._data = {}
+
+  def peek(self):
+    if self._data.has_key(self._head):
+      return self._data[self._head]
+    else:
+      return None
+
+  def size(self):
+    return self._size
+
+  def isEmpty(self):
+    return self.size() == 0
+
+  def enqueue(self, item):
+    if self.isEmpty():
+      self._data[0] = item
+      self._head = 0
+      self._tail = 0
+    else:
+      self._tail += 1
+      self._data[self._tail] = item
+
+    self._size += 1
+
+  def dequeue(self):
+    if not self._data.has_key(self._head):
+      return None
+
+    item = self._data[self._head]
+
+    del self._data[self._head]
+
+    if not self.isEmpty():
+      self._head += 1
+    else:
+      self._head = None
+      self._tail = None
+
+    self._size -= 1
+  
+    return item

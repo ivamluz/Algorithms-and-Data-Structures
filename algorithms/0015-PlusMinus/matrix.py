ef37d64145a1b3275d0c01d9fbe8c5f4c9d69050
Ivam Luz <iluz@ciandt.com>
2018-11-18 23:25:26 -0200

    Implement solution for 0015-PlusMinus

--- algorithms/0015-PlusMinus/matrix.py
+++ algorithms/0015-PlusMinus/matrix.py
@@ -1,3 +1,25 @@
 def plus_minus(arr):
   if not arr:
     return [0, 0, 0]
+
+  count = {
+    'positives': 0,
+    'negatives': 0,
+    'zeros': 0
+  }
+
+  for i in arr:
+    if i > 0:
+      count['positives'] += 1
+    elif i < 0:
+      count['negatives'] += 1
+    else:
+      count['zeros'] += 1
+
+  length = float(len(arr))
+
+  return [
+    round(count['positives'] / length, 6),
+    round(count['negatives'] / length, 6),
+    round(count['zeros'] / length, 6),
+  ]
--- algorithms/0015-PlusMinus/test_plus_minus.py
+++ algorithms/0015-PlusMinus/test_plus_minus.py
@@ -8,5 +8,14 @@ class TestPlusMinus(unittest.TestCase):
   def test_empty_array(self):
     self.assertEquals(plus_minus([]), [0, 0, 0])
 
+  def test_desc_sorted_array(self):
+    self.assertEquals(plus_minus([1, 1, 0, -1, -1]), [0.400000, 0.400000, 0.200000])
+
+  def test_asc_sorted_array(self):
+    self.assertEquals(plus_minus([-1, -1, 0, 1, 1]), [0.400000, 0.400000, 0.200000])
+
+  def test_unsorted_array(self):
+    self.assertEquals(plus_minus([-4, 3, -9, 0, 4, 1]), [0.500000, 0.333333, 0.166667])
+
 if __name__ == '__main__':
   unittest.main()

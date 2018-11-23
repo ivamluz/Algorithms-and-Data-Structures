1edba9b95b84d2107c6cb9a516bdec6ee99bdc4e
ivamluz <ivam.santos@gmail.com>
2018-11-23 20:07:53 -0200

    Implement solution for algorithms/0018-TimeConversion challenge

--- algorithms/0018-TimeConversion/test_convert_time.py
+++ algorithms/0018-TimeConversion/test_convert_time.py
@@ -2,7 +2,23 @@ import unittest
 from time_utils import convert_time
 
 class TestConvertTime(unittest.TestCase):
-    pass
+    def testNoneValue(self):
+        self.assertEquals(convert_time(None), '')
+
+    def testEmptyValue(self):
+        self.assertEquals(convert_time(''), '')
+
+    def testAmTimeConversion(self):
+        self.assertEquals(convert_time('07:05:45AM'), '07:05:45')
+
+    def testPmTimeConversion(self):
+        self.assertEquals(convert_time('07:05:45PM'), '19:05:45')
+
+    def testMidnightTimeConversion(self):
+        self.assertEquals(convert_time('12:30:10AM'), '00:30:10')
+
+    def testNoonTimeConversion(self):
+        self.assertEquals(convert_time('12:30:10PM'), '12:30:10')
 
 if __name__ == '__main__':
     unittest.main()
--- algorithms/0018-TimeConversion/time_utils.py
+++ algorithms/0018-TimeConversion/time_utils.py
@@ -1,2 +1,31 @@
+import re
+
 def convert_time(am_pm_time):
-    pass
+    if not am_pm_time:
+        return ''
+
+    matches = re.match( r'^([0-1][0-9]):([0-5][0-9]):([0-5][0-9])(AM|PM)$', am_pm_time)
+
+    if not matches:
+        return ''
+
+    hours = matches.group(1)
+    minutes = matches.group(2)
+    seconds = matches.group(3)
+    period = matches.group(4)
+
+    # if hours == '12':
+    #     if period == 'AM':
+    #         hours = '00'
+    #     else:
+    #         hours = str(12 + int(hours))
+
+    if period == 'AM':
+        if hours == '12':
+            hours = '00'
+    elif hours != '12':
+        hours = str(12 + int(hours))
+
+    converted = "%s:%s:%s" % (hours, minutes, seconds)
+
+    return converted

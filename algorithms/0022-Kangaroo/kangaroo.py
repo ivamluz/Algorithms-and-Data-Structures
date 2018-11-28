c05679c4ee942b8caa0c0c5001d6df45cca285e6
ivamluz <ivam.santos@gmail.com>
2018-11-28 21:53:11 -0200

    Solve algorithms/0022-Kangaroo challenge

--- algorithms/0022-Kangaroo/kangaroo.py
+++ algorithms/0022-Kangaroo/kangaroo.py
@@ -2,4 +2,13 @@ KANGOROO_RESULT_YES = 'YES'
 KANGOROO_RESULT_NO = 'NO'
 
 def kangaroo(x1, v1, x2, v2):
-    return KANGOROO_RESULT_YES
+    arithmetic_progression = lambda first, difference, n: first + (n - 1) * difference
+
+    for i in range(2, 10000):
+        kangaroo1_position = arithmetic_progression(x1, v1, i)
+        kangaroo2_position = arithmetic_progression(x2, v2, i)
+
+        if kangaroo1_position == kangaroo2_position:
+            return 'YES'
+
+    return 'NO'

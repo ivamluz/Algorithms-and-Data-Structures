846bc7571379fff8f5de5e6657224e8de7590c5e
ivamluz <ivam.santos@gmail.com>
2018-12-04 22:32:53 -0200

    Refactor algoritms/0023-FindDigits

--- algorithms/0023-FindDigits/digits.py
+++ algorithms/0023-FindDigits/digits.py
@@ -2,10 +2,8 @@ def findDigits(number):
     # one liner
     # return sum(map(lambda x: 1 if int(x) > 0 and number % int(x) == 0 else 0, str(number)))
 
-    digits = [int(x) for x in str(number)]
-    
     count = 0
-    for digit in digits:
+    for digit in [int(d) for d in str(number)]:
         if digit > 0 and number % digit == 0:
             count += 1
 
